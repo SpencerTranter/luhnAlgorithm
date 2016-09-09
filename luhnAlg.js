16adff757536467be24f88ed23d4d2b4802aec62
Spencer Tranter <spencertranter95@gmail.com>
2016-09-09 22:45:51 +0000

    Added working functions that passed tests

--- luhnAlg.js
+++ luhnAlg.js
@@ -1,28 +1,51 @@
-var luhn = {
+var args = process.argv;
+var stringArray = args[2].split('');
+var numArray = toNumArr(stringArray);
 
+var isLuhn = {
 
   doubleSecond: function(array){
-    for(i = 0; i < array.length; i + 2){
-      if(array[i] * 2 > 9){
-        array[i] = (array[i] * 2) - 9;
+    var newArray = [];
+    for(i =  array.length - 1; i >= 0; i -= 1){
+      if(i % 2 === 0){
+        newArray[i] = array[i];
+      } else if((array[i] * 2) > 9){
+        newArray[i] = (array[i] * 2) - 9;
+      } else {
+        newArray[i] = array[i] * 2;
       }
     }
-    return array;
+    return newArray;
   },
 
-  lessThanTen: function(array){
-
-  }
-
   findX: function(array){
+    var additions = 0;
+    array.map(function(curr){
+      additions += curr;
+    })
+    return ((additions * 9) % 10);
 
+  },
+
+  isArrayLuhn: function(array){
+    var additions = 0;
+    array.map(function(curr){
+      additions += curr;
+    })
+    return ((additions * 9) % 10) === 0;
   }
-
-  isLuhn: function(array){
-
-  }
-
-
-
 }
-module.exports = isPalindrome;
+
+function toNumArr (stringArray){
+  for(var i=0; i<stringArray.length; i++) {
+    stringArray[i] = +stringArray[i];
+  }
+  console.log(stringArray);
+  return stringArray;
+}
+
+
+module.exports = isLuhn;
+finalArray = numArray.push(isLuhn.findX(isLuhn.doubleSecond(numArray)));
+console.log(isLuhn.isArrayLuhn(isLuhn.doubleSecond(finalArray)));
+//console.log(numArray);
